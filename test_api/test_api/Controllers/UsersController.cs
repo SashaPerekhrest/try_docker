8ae7ca798c957609d1b085b9998709be46a9fc7d
SashaPerekhrest <65830443+SashaPerekhrest@users.noreply.github.com>
2024-05-16 01:52:03 +0500

    init

---
+++ test_api/test_api/Controllers/UsersController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace test_api.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetUsers()
+        {
+            var users = new[]
+            {
+                new {Name = "Sasha"},
+                new {Name = "Vachic" }
+            };
+
+            return Ok(users);
+        }
+    }
+}
